670710d3ef54bb71da14577da57b28cefba367d2
josephlinden <55002647+josephlinden@users.noreply.github.com>
2019-09-06 23:16:08 +0200

    Create ext_emconf.php

---
+++ ext_emconf.php
@@ -0,0 +1,19 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Typo3CodeCoverageBugExt',
+	'description' => '',
+	'category' => 'plugin',
+	'author' => 'Joseph Linden',
+    'state' => 'stable',
+	'internal' => '',
+	'uploadfolder' => '0',
+	'createDirs' => '',
+	'clearCacheOnLoad' => 0,
+    'version' => '0.0.1',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '7.6.0-8.9.99',
+		),
+	),
+);
